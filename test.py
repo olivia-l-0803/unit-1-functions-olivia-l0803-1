ed374812d4da863f6647cd42696c80d94e652d5d
olivia-l0803 <olivialu415@gmail.com>
2025-09-08 22:33:46 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+print("test")
+Turtle.speed('fastest')
+
+def mysisterGOONS():
+    for i in range(200):
+        t.forward(i * 3)
+        t.right(90)
+Turtle.done()
+
+mysisterGOONS()
+Turtle.done()
